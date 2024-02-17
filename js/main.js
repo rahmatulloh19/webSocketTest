9453ed5dbf68200ee68c1ecdb4c4eb0eebd7991a
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-02-18 02:11:29 +0500

    finished web socket

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-fetch("http://localhost:8000/todo/")
-	.then((res) => res.json())
-	.then((data) => console.log(data));
+const elForm = document.querySelector("form");
+const elDiv = document.querySelector("div");
+
+const socket = new WebSocket("ws://localhost:5001");
+
+elForm.addEventListener("submit", (evt) => {
+	evt.preventDefault();
+	socket.send(evt.target[0].value.trim());
+	evt.target[0].value = "";
+	evt.target[0].focus();
+});
+
+socket.addEventListener("message", ({ data }) => {
+	const elP = document.createElement("p");
+
+	elP.textContent = data;
+	elDiv.appendChild(elP);
+});
---
+++ server/index.js
@@ -0,0 +1,8 @@
+const ws = require("ws");
+const server = new ws.Server({ port: 5001 });
+
+server.on("connection", (socket) => {
+	socket.on("message", (message) => {
+		socket.send(`${message}`);
+	});
+});
