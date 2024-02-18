2410c2ad305bfc1550e6d00736fda659ea7fc0e3
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-02-19 00:12:39 +0500

    add some change

--- js/main.js
+++ js/main.js
@@ -3,11 +3,22 @@ const elDiv = document.querySelector("div");
 
 const socket = io("ws://localhost:3000");
 
+let uniqueId;
+
+socket.on("clientJoined", async (data) => {
+	uniqueId = await data;
+	console.log(uniqueId);
+});
+
 elForm.addEventListener("submit", (evt) => {
 	evt.preventDefault();
 	socket.emit("clientJoined", {
 		roomId: (Math.random() * 100000).toFixed(),
 		message: evt.target[0].value.trim(),
+	});
+
+	socket.on("clientJoined", (data) => {
+		console.log(data);
 	});
 	// socket.on("message", () => {
 	// 	const elP = document.createElement("p");
